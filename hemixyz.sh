74a3fede1adf98ea63125decae3b7f850d650015
Stephen Ahmeed <118715242+stephenahmeed@users.noreply.github.com>
2024-11-21 02:00:48 +0600

    Update hemixyz.sh

--- hemixyz.sh
+++ hemixyz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/zunxbt/logo/main/logo.sh | bash
+curl -s https://raw.githubusercontent.com/stephenahmeed/logo/main/logo.sh | bash
 
 sleep 3
 
